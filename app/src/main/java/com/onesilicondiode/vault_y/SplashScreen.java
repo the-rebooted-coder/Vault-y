0d28800b38cdb761e6eda113a243870fb2f1810b
Spandan Saxena <spandansaxena@gmail.com>
2021-12-03 00:51:48 +0530

    Added Perfect Splash

--- app/src/main/java/com/onesilicondiode/vault_y/SplashScreen.java
+++ app/src/main/java/com/onesilicondiode/vault_y/SplashScreen.java
@@ -10,20 +10,22 @@ import android.os.Bundle;
 @SuppressLint("CustomSplashScreen")
 public class SplashScreen extends AppCompatActivity {
     public static final String USER_CODE = "userPin";
+    int anshu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         SharedPreferences prefs = getSharedPreferences(USER_CODE, MODE_PRIVATE);
-        if (prefs == null){
-            //user pin not set, proceed to setting
-            Intent toSignUp = new Intent(this,SignUp.class);
-            startActivity(toSignUp);
-            finish();
-        }
-        else {
+        anshu = prefs.getInt("userPin",0);
+        if (anshu !=0){
             //user pin set, proceed to main-activity
             Intent toMain = new Intent(this,MainActivity.class);
             startActivity(toMain);
+            finish();
+        }
+        else {
+            //user pin not set, proceed to setting
+            Intent toSignUp = new Intent(this,SignUp.class);
+            startActivity(toSignUp);
             finish();
         }
     }
