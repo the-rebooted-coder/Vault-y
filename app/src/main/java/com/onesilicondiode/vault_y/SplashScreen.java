7c8ea092e4655b1ffce124bbb39fe1a3e8b2dcda
Spandan Saxena <spandansaxena@gmail.com>
2021-12-03 00:10:03 +0530

    New Splash Screen

--- app/src/main/java/com/onesilicondiode/vault_y/SplashScreen.java
+++ app/src/main/java/com/onesilicondiode/vault_y/SplashScreen.java
@@ -2,13 +2,29 @@ package com.onesilicondiode.vault_y;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashScreen extends AppCompatActivity {
-
+    public static final String USER_CODE = "userPin";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        SharedPreferences prefs = getSharedPreferences(USER_CODE, MODE_PRIVATE);
+        if (prefs == null){
+            //user pin not set, proceed to setting
+            Intent toSignUp = new Intent(this,SignUp.class);
+            startActivity(toSignUp);
+            finish();
+        }
+        else {
+            //user pin set, proceed to main-activity
+            Intent toMain = new Intent(this,MainActivity.class);
+            startActivity(toMain);
+            finish();
+        }
     }
 }
