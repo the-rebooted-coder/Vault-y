55b228e2a14d7dcacf90c53feb721e9f8070fc45
Spandan Saxena <spandansaxena@gmail.com>
2021-12-04 03:18:48 +0530

    Added Logic for Securing and Encrypting MPIN

--- app/src/main/java/com/onesilicondiode/vault_y/Securing.java
+++ app/src/main/java/com/onesilicondiode/vault_y/Securing.java
@@ -7,6 +7,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.widget.Toast;
 
 public class Securing extends AppCompatActivity {
@@ -16,10 +17,6 @@ public class Securing extends AppCompatActivity {
     int binary2;
     int binary3;
     int binary4;
-    int binary5;
-    int binary6;
-    int binary7;
-    int binary8;
     int splitValue1;
     int splitValue2;
     int splitValue3;
@@ -46,14 +43,83 @@ public class Securing extends AppCompatActivity {
     private void startEncryptionProcess() {
         splitMPIN();
         convertToBinary();
+        Log.i("Generated Keypair",binary1+"");
+        Log.i("Generated Keypair",binary2+"");
+        Log.i("Generated Keypair",binary3+"");
+        Log.i("Generated Keypair",binary4+"");
     }
 
     private void convertToBinary() {
-        if (splitValue1 == 1){
-            
+       splitValueOne();
+       splitValueTwo();
+       splitValueThree();
+       splitValueFour();
+    }
+    private void splitValueOne() {
+        if (splitValue1== 0){
+            binary1 = 0000;
+        }
+        else if (splitValue1 == 1){
+            binary1 = 0001;
+        }
+        else if (splitValue1 == 2){
+            binary1 = 1011;
+        }
+        else if (splitValue1 == 3){
+            binary1 = 0011;
+        }
+        else if (splitValue1 == 4){
+            binary1 = 0101;
+        }
+        else if (splitValue1 == 5){
+            binary1 = 0011;
+        }
+        else if (splitValue1 == 6){
+            binary1 = 1100;
+        }
+        else if (splitValue1 == 7){
+            binary1 = 1110;
+        }
+        else if (splitValue1 == 8){
+            binary1 = 1010;
+        }
+        else if (splitValue1 == 9){
+            binary1 = 0111;
         }
     }
-
+    private void splitValueTwo() {
+        if (splitValue2 <4){
+            binary2 = 00;
+        }
+        else if (splitValue2 < 7){
+            binary2 = 01;
+        }
+        else if (splitValue2 <10){
+            binary2 = 10;
+        }
+    }
+    private void splitValueThree() {
+        if (splitValue3 <4){
+            binary3 = 10;
+        }
+        else if (splitValue3 < 7){
+            binary3 = 00;
+        }
+        else if (splitValue3 <10){
+            binary3 = 01;
+        }
+    }
+    private void splitValueFour() {
+        if (splitValue4 <4){
+            binary4 = 01;
+        }
+        else if (splitValue4 < 7){
+            binary4 = 10;
+        }
+        else if (splitValue4 <10){
+            binary4 = 00;
+        }
+    }
     private void splitMPIN() {
            splitValue1 = securedKey.charAt(0);
            splitValue2 = securedKey.charAt(1);
