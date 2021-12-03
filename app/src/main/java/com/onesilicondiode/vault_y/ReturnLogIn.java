48c023a599b2adf9503abaabe3a54063ea2136ca
Spandan Saxena <spandansaxena@gmail.com>
2021-12-03 20:02:12 +0530

    Created XML of return

--- app/src/main/java/com/onesilicondiode/vault_y/ReturnLogIn.java
+++ app/src/main/java/com/onesilicondiode/vault_y/ReturnLogIn.java
@@ -10,5 +10,6 @@ public class ReturnLogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_return_log_in);
+
     }
 }
