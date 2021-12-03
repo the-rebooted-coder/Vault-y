a26c5c73a9350a46c212d764a6c8af9b0c62ca0d
Spandan Saxena <spandansaxena@gmail.com>
2021-12-04 03:25:01 +0530

    Added Logic for Returning User

--- app/src/main/java/com/onesilicondiode/vault_y/ReturnLogIn.java
+++ app/src/main/java/com/onesilicondiode/vault_y/ReturnLogIn.java
@@ -4,16 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
 public class ReturnLogIn extends AppCompatActivity {
     MaterialButton login;
+    public static final String USER_CODE = "userPin";
+    EditText userEnteredPin;
+    String securedKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,22 +27,31 @@ public class ReturnLogIn extends AppCompatActivity {
         setContentView(R.layout.activity_return_log_in);
         init();
         actions();
+        getKeyValue();
     }
 
     private void actions() {
         login.setOnClickListener(view -> {
-            haptics();
-            Intent toMain = new Intent(ReturnLogIn.this,MainActivity.class);
-            startActivity(toMain);
-            finish();
-            overridePendingTransition(R.anim.fadein,R.anim.fadeout);
+            if (userEnteredPin.getText().toString().equals(securedKey)){
+                //User Entered Correct PIN proceed to Vault
+                CorrectHaptics();
+                Intent toMain = new Intent(ReturnLogIn.this,MainActivity.class);
+                startActivity(toMain);
+                finish();
+                overridePendingTransition(R.anim.fadein,R.anim.fadeout);
+            }
+            else {
+                WrongHaptics();
+                Toast.makeText(this,"Wrong MPIN Entered",Toast.LENGTH_SHORT).show();
+            }
         });
     }
 
     private void init(){
         login = findViewById(R.id.enterVaultAgain);
+        userEnteredPin = findViewById(R.id.userEnteredmpin);
     }
-    private void haptics() {
+    private void CorrectHaptics() {
         Vibrator v3 = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
         long[] pattern = {0,25,50,35,100};
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -44,5 +59,18 @@ public class ReturnLogIn extends AppCompatActivity {
         } else {
             v3.vibrate(pattern,-1);
         }
+    }
+    private void WrongHaptics() {
+        Vibrator v3 = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+        long[] pattern = {0,10,25,40,55,70,85,100};
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v3.vibrate(VibrationEffect.createWaveform(pattern,-1));
+        } else {
+            v3.vibrate(pattern,-1);
+        }
+    }
+    private void getKeyValue() {
+        SharedPreferences prefs = getSharedPreferences(USER_CODE, MODE_PRIVATE);
+        securedKey = prefs.getString("userPin",null);
     }
 }
