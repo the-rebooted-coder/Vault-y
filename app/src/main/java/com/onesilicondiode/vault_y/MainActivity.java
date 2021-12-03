319c64cab80cd79eb30f5412475dc855f2659320
Spandan Saxena <spandansaxena@gmail.com>
2021-12-03 20:24:13 +0530

    Proper logic for maintaining Return LifeCycle

--- app/src/main/java/com/onesilicondiode/vault_y/MainActivity.java
+++ app/src/main/java/com/onesilicondiode/vault_y/MainActivity.java
@@ -6,11 +6,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    boolean isSessionActive = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,24 +21,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    boolean doubleBackToExitPressedOnce = false;
     @Override
-    protected void onStart() {
-        super.onStart();
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Back again to close Vault-y", Toast.LENGTH_SHORT).show();
+        new Handler(Looper.getMainLooper()).postDelayed(() -> doubleBackToExitPressedOnce=false, 2000);
     }
-
     @Override
     protected void onPause() {
         super.onPause();
         haptics();
-        Toast.makeText(this,"Securely Closing Vault-y",Toast.LENGTH_SHORT).show();
+        isSessionActive = false;
+        Toast.makeText(this,"Securely closing Vault-y",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Intent toReturn = new Intent(this,ReturnLogIn.class);
-        startActivity(toReturn);
-        overridePendingTransition(R.anim.fadein,R.anim.fadeout);
+        if (!isSessionActive) {
+            Intent toReturn = new Intent(this,ReturnLogIn.class);
+            startActivity(toReturn);
+            overridePendingTransition(R.anim.fadein,R.anim.fadeout);
+        }
     }
     private void haptics() {
         Vibrator v3 = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
