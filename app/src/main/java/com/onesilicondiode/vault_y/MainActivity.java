610874628b18a29cd857d6d3d86d0254eeb7cce9
Spandan Saxena <spandansaxena@gmail.com>
2021-12-03 20:06:31 +0530

    Added Logic to onResume and onPause Method

--- app/src/main/java/com/onesilicondiode/vault_y/MainActivity.java
+++ app/src/main/java/com/onesilicondiode/vault_y/MainActivity.java
@@ -2,7 +2,9 @@ package com.onesilicondiode.vault_y;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this,"Securely Closing Vault-y",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent toReturn = new Intent(this,ReturnLogIn.class);
+        startActivity(toReturn);
+        overridePendingTransition(R.anim.fadein,R.anim.fadeout);
     }
 }
