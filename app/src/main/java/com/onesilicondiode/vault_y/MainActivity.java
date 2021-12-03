3b3099369a42e8f0832423d766d29dd76d9d18db
Spandan Saxena <spandansaxena@gmail.com>
2021-12-03 20:11:22 +0530

    Added dummy logic for Return Activity

--- app/src/main/java/com/onesilicondiode/vault_y/MainActivity.java
+++ app/src/main/java/com/onesilicondiode/vault_y/MainActivity.java
@@ -2,8 +2,12 @@ package com.onesilicondiode.vault_y;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        haptics();
         Toast.makeText(this,"Securely Closing Vault-y",Toast.LENGTH_SHORT).show();
     }
 
@@ -26,5 +31,14 @@ public class MainActivity extends AppCompatActivity {
         Intent toReturn = new Intent(this,ReturnLogIn.class);
         startActivity(toReturn);
         overridePendingTransition(R.anim.fadein,R.anim.fadeout);
+    }
+    private void haptics() {
+        Vibrator v3 = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+        long[] pattern = {0,25,50,35,100};
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v3.vibrate(VibrationEffect.createWaveform(pattern,-1));
+        } else {
+            v3.vibrate(pattern,-1);
+        }
     }
 }
--- app/src/main/java/com/onesilicondiode/vault_y/ReturnLogIn.java
+++ app/src/main/java/com/onesilicondiode/vault_y/ReturnLogIn.java
@@ -2,14 +2,49 @@ package com.onesilicondiode.vault_y;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class ReturnLogIn extends AppCompatActivity {
+    MaterialButton login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_return_log_in);
+        init();
+        actions();
+    }
 
+    private void actions() {
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                haptics();
+                Intent toMain = new Intent(ReturnLogIn.this,MainActivity.class);
+                startActivity(toMain);
+                overridePendingTransition(R.anim.fadein,R.anim.fadeout);
+            }
+        });
+    }
+
+    private void init(){
+        login = findViewById(R.id.enterVaultAgain);
+    }
+    private void haptics() {
+        Vibrator v3 = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+        long[] pattern = {0,25,50,35,100};
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v3.vibrate(VibrationEffect.createWaveform(pattern,-1));
+        } else {
+            v3.vibrate(pattern,-1);
+        }
     }
 }
