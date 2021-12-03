efceb2b657425579e840b163bb35d5890a9b09a1
Spandan Saxena <spandansaxena@gmail.com>
2021-12-03 19:54:21 +0530

    Created Return Activity

---
+++ app/src/main/java/com/onesilicondiode/vault_y/ReturnLogIn.java
@@ -0,0 +1,14 @@
+package com.onesilicondiode.vault_y;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReturnLogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_return_log_in);
+    }
+}
