371a9205d50b95f310187b86cdfc01a2f40e993d
Spandan Saxena <spandansaxena@gmail.com>
2021-12-05 03:55:43 +0530

    Added UX and main logic to Fingerprint

--- app/src/main/java/com/onesilicondiode/vault_y/FingerprintHandler.java
+++ app/src/main/java/com/onesilicondiode/vault_y/FingerprintHandler.java
@@ -5,10 +5,14 @@ import android.content.Context;
 import android.hardware.fingerprint.FingerprintManager;
 
 import android.os.CancellationSignal;
+import android.os.Handler;
+import android.os.Looper;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.core.content.ContextCompat;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class FingerprintHandler extends FingerprintManager.AuthenticationCallback {
     private Context context;
@@ -27,15 +31,26 @@ public class FingerprintHandler extends FingerprintManager.AuthenticationCallbac
 
     private void updateUser(String s, boolean b) {
         TextView fingerprintText = (TextView) ((Activity)context).findViewById(R.id.aboutFingerprint);
-        ImageView finger = (ImageView) ((Activity)context).findViewById(R.id.fingerprintView);
+        LottieAnimationView finger = (LottieAnimationView) ((Activity)context).findViewById(R.id.fingerPrintAnim);
         fingerprintText.setText(s);
         if (!b){
             fingerprintText.setTextColor(ContextCompat.getColor(context,R.color.red));
-            finger.setImageResource(R.drawable.ic_baseline_cancel);
+            finger.setAnimation("finger_fail.json");
+            finger.playAnimation();
+            finger.setRepeatCount(0);
+            final Handler handler = new Handler(Looper.getMainLooper());
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                   fingerprintText.setText(R.string.use_finger);
+                }
+            }, 4000);
         }
         else {
             fingerprintText.setTextColor(ContextCompat.getColor(context,R.color.green));
-            finger.setImageResource(R.drawable.ic_baseline_check_pass);
+            finger.setAnimation("finger_pass.json");
+            finger.playAnimation();
+            finger.setRepeatCount(0);
         }
     }
 
