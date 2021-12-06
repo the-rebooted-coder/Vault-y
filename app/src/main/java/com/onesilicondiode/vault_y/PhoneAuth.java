f4be36272ce8c6b9cbb64c827af0b1d6f0f485f3
Spandan Saxena <spandansaxena@gmail.com>
2021-12-06 18:49:39 +0530

    Created Phone Auth

---
+++ app/src/main/java/com/onesilicondiode/vault_y/PhoneAuth.java
@@ -0,0 +1,14 @@
+package com.onesilicondiode.vault_y;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhoneAuth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_auth);
+    }
+}
